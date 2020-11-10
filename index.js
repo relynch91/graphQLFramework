fa110a283d9ef2dd2a12d9f994376d655c3f1228
Ryan Lynch <ReLynch91@gmail.com>
2020-11-09 22:31:02 -0800

    config mongoose db and express-graphql server

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const app = express();
+const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const { graphqlHTTP } = require('express-graphql');
+
+app.use(
+    "/graphql",
+    graphqlHTTP({
+        graphiql: true
+    })
+);
+
+mongoose
+    .connect(db)
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World"));
+
+app.use(bodyParser.json());
+
+// all requests coming in to `graphql` will be handled
+// by the expressGraphQL function from the 'express-graphql' library
 
 app.listen(5000, () => console.log("Server is running on Port 5000"));
